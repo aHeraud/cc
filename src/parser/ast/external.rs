5000100119784a1dc3bc76b82ba4bf2b074f8099
Achille Heraud <achille@heraud.xyz>
2019-01-09 22:23:15 -0600

    Added statement ast & parse rules

--- src/parser/ast/external.rs
+++ src/parser/ast/external.rs
@@ -4,13 +4,16 @@ pub type TranslationUnit = Vec<ExternalDeclaration>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum ExternalDeclaration {
-    FunctionDefinition(FunctionDefinition)
+    FunctionDefinition(FunctionDefinition),
+    Declaration(Declaration)
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub struct FunctionDefinition {
     pub declaration_specifiers: DeclarationSpecifiers,
     pub declarator: Declarator,
-    //pub declaration_list: DeclarationList,
+    pub declaration_list: DeclarationList,
     pub compound_statement: CompoundStatement
 }
+
+pub type DeclarationList = Vec<Declaration>;
--- src/parser/ast/statements.rs
+++ src/parser/ast/statements.rs
@@ -4,16 +4,45 @@ pub type CompoundStatement = Vec<BlockItem>;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum BlockItem {
-    Statement(Statement)
+    Declaration(Declaration),
+    Statement(Box<Statement>)
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum Statement {
-    Jump(JumpStatement)
+    Jump(JumpStatement),
+    Labeled(LabeledStatement),
+    Compound(CompoundStatement),
+    Expression(Expression),
+    Selection(SelectionStatement),
+    Iteration(IterationStatement)
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum LabeledStatement {
+    Statement{ label: String, statement: Box<Statement> },
+    Case { value: ConstantExpression, body: Box<Statement> },
+    DefaultCase { body: Box<Statement> }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum SelectionStatement {
+    If { condition: Expression, body: Box<Statement>, else_clause: Option<Box<Statement>> },
+    Switch { condition: Expression, body: Box<Statement> }
+}
+
+#[derive(Debug, Clone, PartialEq, Eq)]
+pub enum IterationStatement {
+    While { condition: Expression, body: Box<Statement> },
+    DoWhile { body: Box<Statement>, condition: Expression },
+    ForA{ expr1: Option<Expression>, condition: Option<Expression>, expr3: Option<Expression>, body: Box<Statement> },
+    ForB{ clause1: Declaration, condition: Option<Expression>, expr3: Option<Expression>, body: Box<Statement> }
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum JumpStatement {
+    Goto(String),
+    Continue,
+    Break,
     Return(Option<Box<Expression>>)
 }
-
